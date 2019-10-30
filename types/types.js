bec6728a7926590e4bfa64b93a2858aae1959779
doktodirk <doktordirk@outlook.de>
2019-10-30 23:48:12 +0100

    pre-build 4.4.0

---
+++ types/types.js
@@ -0,0 +1,5 @@
+"use strict";
+// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ0eXBlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsa0RBQWtEO0FBQ2xELDhEQUE4RCJ9
