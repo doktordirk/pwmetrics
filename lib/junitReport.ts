aadf89a5ad3d0e129661269deb8c9bf1a230cde7
doktodirk <doktordirk@outlook.de>
2019-10-31 23:04:06 +0100

    Better comply to junit specs
    https://raw.githubusercontent.com/windyroad/JUnit-Schema/master/JUnit.xsd

--- lib/junitReport.ts
+++ lib/junitReport.ts
@@ -13,31 +13,30 @@ export const getTestSuite = (testName: string, url: string, testDuration: number
 
     if (!expectationValue) return;
 
-    testCount =+ 1;
-    const testDesc = `${metricName} should stay bellow ${expectationValue.warn} ms`
+    testCount += 1;
+    const testDesc = `Time till ${metricName} should stay bellow ${expectationValue.warn} ms`
     let type: 'ERROR' | 'WARNING' | 'SUCCESS';
 
     if (metricValue >= expectationValue.error) {
       type = 'ERROR';
       failureCount++;
       const msg = getJUnitAssertionMessage(type, metricName, expectationValue.error, metricValue);
-      testcaseXMLLines.push(`<testcase id="${metricName}" name="${testDesc}" time="${metricValue}">`);
-      testcaseXMLLines.push(`<failure message="${testDesc}" type="${type}">${type} ${msg}</failure>`);
+      testcaseXMLLines.push(`<testcase name="${testDesc}" classname="${testName}" time="${metricValue / 1000}">`);
+      testcaseXMLLines.push(`<failure message="${msg}" type="${type}">${type} ${msg}</failure>`);
       testcaseXMLLines.push('</testcase>');
     } else if (metricValue >= expectationValue.warn && metricValue < expectationValue.error) {
       type = 'WARNING';
       const msg = getJUnitAssertionMessage(type, metricName, expectationValue.warn, metricValue);
-      testcaseXMLLines.push(`<testcase id="${metricName}" name="${testDesc}" time="${metricValue}">`);
-      testcaseXMLLines.push(`<failure message="${testDesc}" type="${type}">${type} ${msg}</failure>`);
+      testcaseXMLLines.push(`<testcase name="${testDesc}" classname="${testName}" time="${metricValue / 1000}">`);
+      testcaseXMLLines.push(`<failure message="${msg}" type="${type}">${type} ${msg}</failure>`);
       testcaseXMLLines.push('</testcase>');
     } else {
-      type = 'SUCCESS';
-      testcaseXMLLines.push(`<testcase id="${metricName}" name="${testDesc}" time="${metricValue}"/>`);
+      testcaseXMLLines.push(`<testcase name="${testDesc}" classname="${testName}" time="${metricValue / 1000}"/>`);
     }
   });
 
   const testsuiteXML = [
-    `<testsuite id="${testName}" name="Testing metrics for ${url}" tests="${testCount}" failures="${failureCount}" time="${testDuration / 1000}">`,
+    `<testsuite id="0" name="${testName}" tests="${testCount}" failures="${failureCount}" errors="0" skipped="0" time="${testDuration / 1000}" hostname="${url}" timestamp="${(new Date()).toISOString()}">`,
     ...testcaseXMLLines,
     '</testsuite>'
   ].join('\n');
